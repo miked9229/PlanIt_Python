f027c08f0773526a17ba32594d6fbbad88787f6f
Michael Doroff <michael_doroff@yahoo.com>
2020-05-12 17:17:40 -0400

    added ability to push code to MongoDB database

--- scraper.py
+++ scraper.py
@@ -1,12 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import pymongo
+import datetime
+try: 
+    client = pymongo.MongoClient("mongodb://planitadmin:planitadmin123@ds011331.mlab.com:11331/heroku_kmsgdv8f?retryWrites=false")
+    print("Connected successfully!!!") 
+except:   
+    print("Could not connect to MongoDB") 
 
-URL = 'https://www.chicagoparkdistrict.com/parks-facilities/?title=&page=1'
+# database 
+db = client['heroku_kmsgdv8f']
+
+collection = db['park']
+
+
+mongoDBPark = db.park
+
+
+URL = 'https://www.chicagoparkdistrict.com/parks-facilities/?title=&page=2'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
 results = soup.find(id='main-page-content')
+
 
 parks = results.find_all('article', class_= 'thumbnail-object')
 
@@ -15,23 +33,29 @@ for park in parks:
     address = park.find('p', class_='address')
     phone = park.find('div', class_='field field--name-field-phone-number field--type-string field--label-hidden field--item')
 
+    stripped_name = ""
+    stripped_address = ""
+    stripped_phone = ""
+
+    if name != None:
+        stripped_name = name.text.strip()
 
 
-    if None in (name, address, phone):
-        continue
+    if address != None:
+        stripped_address = address.text.strip()
 
-try:
-    print(name.text.strip())
-except:
-    print("")
+    if phone != None:
+        stripped_phone = phone.text.strip()
 
-try:
-    print(address.text.strip())
-except:
-    print("")
-try:
-    print(phone.text.strip())
+    park_from_web = {
+        "name": stripped_name,
+        "address": stripped_address,
+        "phone": stripped_phone
+    }
 
-except:
-    print("")
- 
+    print(park_from_web)
+
+    mongoDBPark.insert_one(park_from_web)
+
+
+
