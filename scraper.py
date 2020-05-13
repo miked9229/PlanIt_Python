82ef1280592592f9d6c2ae9fb535ef6fd5797d9e
Michael Doroff <michael_doroff@yahoo.com>
2020-05-13 14:06:55 -0400

    Added ability to get all park information

--- scraper.py
+++ scraper.py
@@ -14,48 +14,64 @@ db = client['heroku_kmsgdv8f']
 
 collection = db['park']
 
+x = collection.delete_many({})
+
+print(x.deleted_count, " documents deleted.") 
 
 mongoDBPark = db.park
 
+total_pages = 396
+first_page = 1
 
-URL = 'https://www.chicagoparkdistrict.com/parks-facilities/?title=&page=2'
-page = requests.get(URL)
+for x in range(1, total_pages + 1):
+    URL = 'https://www.chicagoparkdistrict.com/parks-facilities/?title=&page='+str(x)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-results = soup.find(id='main-page-content')
-
-
-parks = results.find_all('article', class_= 'thumbnail-object')
-
-for park in parks:
-    name = park.find('a')
-    address = park.find('p', class_='address')
-    phone = park.find('div', class_='field field--name-field-phone-number field--type-string field--label-hidden field--item')
-
-    stripped_name = ""
-    stripped_address = ""
-    stripped_phone = ""
-
-    if name != None:
-        stripped_name = name.text.strip()
+    results = soup.find(id='main-page-content')
 
 
-    if address != None:
-        stripped_address = address.text.strip()
+    parks = results.find_all('article', class_= 'thumbnail-object')
 
-    if phone != None:
-        stripped_phone = phone.text.strip()
+    for park in parks:
+        photo = park.find('img', class_='img-responsive')
+        name = park.find('a')
+        address = park.find('p', class_='address')
+        phone = park.find('div', class_='field field--name-field-phone-number field--type-string field--label-hidden field--item')
 
-    park_from_web = {
-        "name": stripped_name,
-        "address": stripped_address,
-        "phone": stripped_phone
-    }
+        stripped_name = ""
+        stripped_address = ""
+        stripped_phone = ""
 
-    print(park_from_web)
+        if name != None:
+            stripped_name = name.text.strip()
 
-    mongoDBPark.insert_one(park_from_web)
+
+        if address != None:
+            stripped_address = address.text.strip()
+
+        if phone != None:
+            stripped_phone = phone.text.strip()
+
+        
+
+        park_from_web = {
+            "name": stripped_name,
+            "address": stripped_address,
+            "phone": stripped_phone,
+            "photo": "" if photo == None else photo.get('src')
+        }
+
+        mongoDBPark.insert_one(park_from_web)
+
+print("Script completed")
+
+
+
+
+
+
+
 
 
 
