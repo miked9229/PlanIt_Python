3d56d4cc5c3a056ba6b51a894ebc217a10a0d6b8
Michael Doroff <michael_doroff@yahoo.com>
2020-05-23 14:38:02 -0400

    added web scraping

--- ParkScraper.py
+++ ParkScraper.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import json
 import pymongo
 import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 try: 
     client = pymongo.MongoClient("mongodb://planitadmin:planitadmin123@ds011331.mlab.com:11331/heroku_kmsgdv8f?retryWrites=false")
     print("Connected successfully!!!") 
@@ -24,20 +27,41 @@ total_pages = 396
 first_page = 1
 
 for x in range(1, total_pages + 1):
+
     URL = 'https://www.chicagoparkdistrict.com/parks-facilities/?title=&page='+str(x)
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-
     results = soup.find(id='main-page-content')
-
-    
     parks = results.find_all('article', class_= 'thumbnail-object')
 
     for park in parks:
+
         photo = park.find('img', class_='img-responsive')
         name = park.find('a')
         address = park.find('p', class_='address')
         phone = park.find('div', class_='field field--name-field-phone-number field--type-string field--label-hidden field--item')
+
+        driver = webdriver.Firefox()
+        driver.get(URL)
+        elm = driver.find_element_by_xpath('//a[@href="'+name['href']+'"]')
+        elm.click()
+
+
+        parkUrl = driver.current_url
+        parkPage = requests.get(parkUrl)
+        parkSoup = BeautifulSoup(parkPage.content, 'html.parser')
+        parkResult = parkSoup.find_all('tr', class_='office-hours__item odd')
+
+        parkDict = {}
+
+        for foo in parkResult:
+
+            dayRes = foo.find('td', class_='office-hours__item-label').text.strip()
+            hourRes = foo.find('td', class_='office-hours__item-slots').text.strip()
+
+            parkDict[dayRes] = hourRes
+
+        driver.quit()
 
         stripped_name = ""
         stripped_address = ""
@@ -53,13 +77,12 @@ for x in range(1, total_pages + 1):
         if phone != None:
             stripped_phone = phone.text.strip()
 
-        
-
         park_from_web = {
             "name": stripped_name,
             "address": stripped_address,
             "phone": stripped_phone,
-            "photo": "" if photo == None else photo.get('src')
+            "photo": "" if photo == None else photo.get('src'), 
+            "hoursOfOperation": parkDict
         }
 
         mongoDBPark.insert_one(park_from_web)
